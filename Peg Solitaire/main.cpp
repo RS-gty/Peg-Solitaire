c9734f1cf08924116689cac58cb02490def64c7f
RS-gty <18920702372@163.com>
2025-12-04 23:32:16 +0800

    Update

--- Peg Solitaire/main.cpp
+++ Peg Solitaire/main.cpp
@@ -10,27 +10,27 @@ int main()
     fillrectangle(150, 60, 200, 85);
     ExMessage m;
     int trigger = 0;
-    while (1) // 进入无限循环
+    while (1)
     {
-        peekmessage(&m, EM_MOUSE); // 检查是否有鼠标消息，将消息存储在m中
+        peekmessage(&m, EM_MOUSE);
 
-        if (m.message == WM_LBUTTONDOWN) // 如果检测到鼠标左键按下消息
+        if (m.message == WM_LBUTTONDOWN)
         {
             trigger = 1;
         }
-        else if (m.message == WM_LBUTTONUP && trigger == 1) // 如果检测到鼠标左键抬起消息且触发器为1
+        else if (m.message == WM_LBUTTONUP && trigger == 1)
         {
-            trigger = 0; // 重置触发器
-            printf("左键按键...... %d, %d\n", m.x, m.y); // 输出提示信息：左键按键
-            if (m.x >= 150 && m.x <= 200 && m.y >= 60 && m.y <= 85) // 如果鼠标位置在指定矩形区域内
+            trigger = 0;
+            printf("左键按键...... %d, %d\n", m.x, m.y);
+            if (m.x >= 150 && m.x <= 200 && m.y >= 60 && m.y <= 85)
             {
-                printf("点击了矩形区域！\n"); // 输出提示信息：点击了矩形区域
+                printf("点击了矩形区域！\n");
 			}
 		}
-        else if (m.message == WM_RBUTTONDOWN) // 如果检测到鼠标右键按下消息
+        else if (m.message == WM_RBUTTONDOWN)
         {
-            printf("右键按键......\n"); // 输出提示信息：右键按键
-			closegraph(); // 关闭图形窗口
+            printf("右键按键......\n");
+			closegraph();
         }
     }
     return 0;
